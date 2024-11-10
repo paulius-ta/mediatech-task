84cbd6d3cedfeba286285275a2109edc294fcdc6
paulius-ta <paulius.tartilas@gmail.com>
2024-11-10 13:00:22 +0200

    update app with disclaimer & password generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import 'styles/global.scss';
+import PasswordGenerator from 'components/PasswordGenerator/PasswordGenerator.tsx';
 
 function App() {
-  return <div className={'hello'}>hello</div>;
+  return (
+    <>
+      <header className={'header'}></header>
+      <main className={'page-content'}>
+        <p className={'disclaimer'}>
+          Our readers help us create quality content. If you purchase via links on our site, we may
+          receive affiliate commissions. <a href={'/'}>Learn more</a>
+        </p>
+        <section className={'password-generator-section'}>
+          <h1>Generate strong passwords</h1>
+          <p>Upgrade the security of your online accounts.</p>
+          <p>Create strong passwords that are completely random and impossible to guess.</p>
+          <div className={'password-generator-wrapper'}>
+            <PasswordGenerator />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
